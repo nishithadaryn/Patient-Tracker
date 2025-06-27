431877051427d7d8894cfdb13857cad185cf94bf
Nishitha Daryn <nishithadaryn@gmail.com>
2025-06-27 23:11:07 +0530

    commit

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+class Patient(BaseModel):
+    name: str
+    age: int
+    condition: str
+
+class Patients(BaseModel):
+    patients: List[Patient]
+
+app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_db = {"patients": []}
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI is running!"}
+
+@app.get("/patients", response_model=Patients)
+def get_patients():
+    return Patients(patients=memory_db["patients"])
+
+@app.post("/patients")
+def add_patient(patient: Patient):
+    memory_db["patients"].append(patient)
+    return patient
