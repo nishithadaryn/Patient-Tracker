46ffeea151855a497ed0e7ba038203bb08e592e8
Nishitha Daryn <nishithadaryn@gmail.com>
2025-06-27 23:24:58 +0530

    commit3

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
+// frontend/src/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default api;
